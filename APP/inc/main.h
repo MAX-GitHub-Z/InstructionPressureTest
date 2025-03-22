43f4f2a98fea27d8292aba97221371e0a6d1bf64
MAX-GitHub-Z <3014759318@qq.com>
2025-03-22 16:00:56 +0800

    add project

---
+++ APP/inc/main.h
@@ -0,0 +1,31 @@
+/**
+  ************************ Copyright *************** 
+  *           (C) Copyright 2025,txt1994,China.
+  *                  All Rights Reserved
+  *
+  *                 https://github.com/MAX-GitHub-Z/MCU_BootLoader
+  *                 email:3014759318@qq.com
+  *                 QQ   :3014759318
+  *      
+  * FileName     : main.h   
+  * Version      : v1.0     
+  * Author       : MAX-GitHub-Z         
+  * Date         : 2025-03-22         
+  * Description  :    
+  * Function List:  
+  * Toolchain : RealView Development Suite
+  *             RealView Microcontroller Development Kit (MDK)
+  *             ARM Developer Suite (ADS)
+  *             Keil uVision
+  ******************************************************
+**/
+
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+#include "stm32f10x.h"
+
+
+
+#endif
+
---
+++ APP/src/main.c
@@ -0,0 +1,31 @@
+/*************************** Copyright ********************** 
+  *
+  *          (C) Copyright 2025,China.
+  *                    All Rights Reserved
+  *                              
+  *                 https://github.com/MAX-GitHub-Z/MCU_BootLoader
+  *                 email:3014759318@qq.com
+  *                 QQ   :3014759318
+  *    
+  * FileName     : main.c   
+  * Version      : v1.0     
+  * Author       : MAX-GitHub-Z         
+  * Date         : 2025-03-22         
+  * Description  :    
+  * Function List:  
+  * Toolchain : RealView Development Suite
+  *             RealView Microcontroller Development Kit (MDK)
+  *             ARM Developer Suite (ADS)
+  *             Keil uVision
+  ***********************************************************/
+
+#include "main.h"
+
+
+int main(void)
+{
+	
+	while(1);
+	
+}
+
